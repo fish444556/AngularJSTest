cec3bcb259dcda059f3d8b5c779ee7310f746b84
fish444556 <jialinl@mtu.edu>
2017-12-15 14:13:50 -0500

    create controller

--- js/CreateController.js
+++ js/CreateController.js
@@ -1,11 +1,15 @@
 angular.module('app')
-.controller('CreateController',  function($scope) {
-	$scope.test = 'hi';
+.controller('CreateController', function($scope, MailUtility, $location) {
 	$scope.to = '';
+	$scope.subject = '';
+	$scope.content = '';
+	$scope.obj = {};
 
 	$scope.submit = function() {
-		if ($scope.to) {
-			// $scope.to = $scope.to;
-		}
+		$scope.obj['to'] = $scope.to;
+		$scope.obj['subject'] = $scope.subject;
+		$scope.obj['content'] = $scope.content;
+		MailUtility.createMail($scope.obj);
+		$location.path('/');
 	};
 });
--- js/ListController.js
+++ js/ListController.js
@@ -1,8 +1,22 @@
 angular.module('app')
-.controller('ListController', function($scope, MailUtility) {
+.controller('ListController', function($scope, MailUtility, $location) {
+	$scope.checkedMail = new Set();
     $scope.mails = MailUtility.getMails();
 
     $scope.viewMail = function(mail) {
         console.log(mail);
+        console.log($scope.mails)
+        // if ($scope.checkedMail.indexOf(mail.id) > 0) {
+        // 	$scope.checkedMail.re
+        // }
+        // debugger
+        // $location.path('fdsfdfdsfdsfds');
+        if ($scope.checkedMail.has(mail.id)) {
+        	$scope.checkedMail.delete(mail.id);
+        }
+        else {
+        	$scope.checkedMail.add(mail.id);
+        }
+        console.log($scope.checkedMail);
     };
 });
